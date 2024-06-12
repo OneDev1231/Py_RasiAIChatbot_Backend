71dbc7951a70cc64fe116446f3188cc88cfd99f3
OneDev1231 <dev.top.guru@gmail.com>
2024-06-12 04:44:06 -0700

    downgrade python 3.9

--- api/main.py
+++ api/main.py
@@ -15,6 +15,7 @@ load_dotenv()
 app = FastAPI()
 
 allowed_origins = os.getenv("ALLOWED_ORIGINS").split(',')
+print(allowed_origins)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
